866b36711eba4aad10538e3389fadd06fee3c855
Ludvig Kirkeby <ludvig.elias.kirkeby@gmail.com>
2025-02-27 14:21:55 +0100

    First Build

---
+++ .idea/Sorting/Insertion.java
@@ -0,0 +1,14 @@
+package Kattis.Sorting;
+
+public class Insertion extends ComparingAndExchanging {
+
+    protected void sort(Comparable[] a) {
+        for (int i = 0; i < a.length; i++) {
+            for (int j = i; j > 0; j--) {
+                if (super.less(a[j], a[j - 1])) {
+                    super.exchange(a, j, j - 1);
+                }
+            }
+        }
+    }
+}
---
+++ .idea/Sorting/Selection.java
@@ -0,0 +1,16 @@
+package Kattis.Sorting;
+
+public class Selection extends ComparingAndExchanging {
+
+    protected void sort(Comparable[] a) {
+        for (int i = 0; i < a.length; i++) {
+            int min = i;
+            for (int j = i + 1; j < a.length; j++) {
+                if (super.less(a[j], a[min])) {
+                    min = j;
+                }
+                super.exchange(a, i, min);
+            }
+        }
+    }
+}
